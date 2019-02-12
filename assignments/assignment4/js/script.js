cf6003a49f26b74f28575e980dbab15a2d663695
Nomad29 <alexandramelancon@outlook.com>
2019-02-12 18:00:16 -0500

    Ex4: Made the mouth reject the tomato and move the tomato back to its initial position.

--- assignments/assignment4/js/script.js
+++ assignments/assignment4/js/script.js
@@ -30,6 +30,8 @@ function setup() {
   $mouth = $('#mouth');
   // Make it droppable
   $mouth.droppable({
+    accept: "#fly",
+    revert: "#tomato",
     // The drop option specifies a function to call when a drop is completed
     drop: flyDropped
   });
@@ -42,7 +44,9 @@ function setup() {
   // Get the fly element from the page
   $tomato = $('#tomato');
   // Make it draggable
-  $tomato.draggable();
+  $tomato.draggable({
+    revert: true
+  });
 
   // Start up the buzzing of the fly
   buzzSFX.loop = true;
@@ -55,7 +59,7 @@ function setup() {
 // In this instance it can only be the fly (it's the only draggable element).
 // The arguments 'event' and 'ui' are automatically passed by jQuery UI and contain
 // helpful information about the event.
-function flyDropped (event,ui) {
+function flyDropped(event, ui) {
   // When we drop the fly into the mouth we should remove the fly from the page
   // ui contains a reference to the draggable element that was just dropped in ui.draggable
   // .remove() removes the select element from the page
@@ -63,30 +67,29 @@ function flyDropped (event,ui) {
   // We should "close the mouth" by changing its image
   // .attr() lets us change specific attributes on HTML element by specifying the attribute
   // and then what we want to set it to - in this case the 'src' attribute to the closed image
-  $(this).attr('src','assets/images/mouth-closed.png');
+  $(this).attr('src', 'assets/images/mouth-closed.png');
   // Now the fly is gone we should stop its buzzing
   buzzSFX.pause();
   // And start the crunching sound effect of chewing
   crunchSFX.play();
   // Use a setInterval to call the chew() function over and over
-  setInterval(chew,250);
+  setInterval(chew, 250);
 }
 
 // chew()
 //
 // Swaps the mouth image between closed and open and plays the crunching SFX
-function chew () {
+function chew() {
   // We can use .attr() to check the value of an attribute to
   // In this case we check if the image is the open mouth
   if ($mouth.attr('src') === 'assets/images/mouth-open.png') {
     // If it is, we set the 'src' attribute to the closed mouth
-    $mouth.attr('src','assets/images/mouth-closed.png');
+    $mouth.attr('src', 'assets/images/mouth-closed.png');
     // and play the crunching
     crunchSFX.play();
-  }
-  else {
+  } else {
     // Otherwise the 'src' attribute must have been the closed mouth
     // so we swap it for the open mouth
-    $mouth.attr('src','assets/images/mouth-open.png');
+    $mouth.attr('src', 'assets/images/mouth-open.png');
   }
 }
