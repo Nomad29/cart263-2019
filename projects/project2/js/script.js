fb2baacf5c9096a06fe5ab9463b3c2c1185cfdf7
Nomad29 <alexandramelancon@outlook.com>
2019-03-13 20:02:45 -0400

    Prj2: Added the validation function for checking for bad words in the search bar.

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -4,11 +4,11 @@
 
 The Normality Simulator
 Alexandra Melançon
-A fake browsing simulator on how people normally behave with a tool like the
+An interactive experience on how people normally behave with a tool like the
 World Wide Web at their fingertips. Sometimes, the searches are funny, but
 they can also become rapidly twisted. To counter the spreading perversion of
-the WWW, the authorities created the AI named BLACK BOX to control the
-users in a not so distant future.
+the WWW, the authorities created an IA to control the users in a not so
+distant future.
 
 
 Uses:
@@ -22,22 +22,38 @@ let theuser;
 let callCount = 0;
 // List of approximately 100 most vulgar slang words in english (sorry in
 // advance for the vulgarity!)
-let badWords = ['fuck', 'cunt', 'skullfuck', 'felch', 'cum', 'blumpkin', 'cock', 'dick', 'suck', 'hole', 'flap', 'soggy', 'hairy', 'cumdump', 'ass', 'sucker', 'bitch', 'bitchass', 'mother', 'fucker', 'motherfucker', 'cockbag', 'testicle', 'testicles', 'chugger', 'motherfucking', 'clitty', 'felcher', 'analconda', 'fuckmeat', 'bang', 'milf', 'gmilf', 'shit', 'cuntasaurus', 'labe', 'cumjunkie', 'butt', 'fucktoy', 'gangbang', 'anal', 'cocksucker', 'disksucker', 'fucky', 'murder', 'kill', 'pussy', 'sucky', 'pussies', 'killing', 'Fuck', 'Cunt', 'Skullfuck', 'Felch', 'Cum', 'Blumpkin', 'Cock', 'Dick', 'Suck', 'Hole', 'Flap', 'Soggy', 'Hairy', 'Cumdump', 'Ass', 'Sucker', 'Bitch', 'Bitchass', 'Mother', 'Fucker', 'Motherfucker', 'Cockbag', 'Testicle', 'Testicles', 'Chugger', 'Motherfucking', 'Clitty', 'Felcher', 'Analconda', 'Fuckmeat', 'Bang', 'Milf', 'Gmilf', 'Shit', 'Cuntasaurus', 'Labe', 'Cumjunkie', 'Butt', 'Fucktoy', 'Gangbang', 'Anal', 'Cocksucker', 'Disksucker', 'Fucky', 'Murder', 'Kill', 'Pussy', 'Sucky', 'Pussies', 'Killing'];
+let badWords = new Array('fuck', 'cunt', 'skullfuck', 'felch', 'cum', 'blumpkin', 'cock', 'dick', 'suck', 'hole', 'flap', 'soggy', 'hairy', 'cumdump', 'ass', 'sucker', 'bitch', 'bitchass', 'mother', 'fucker', 'motherfucker', 'cockbag', 'testicle', 'testicles', 'chugger', 'motherfucking', 'clitty', 'felcher', 'analconda', 'fuckmeat', 'bang', 'milf', 'gmilf', 'shit', 'cuntasaurus', 'labe', 'cumjunkie', 'butt', 'fucktoy', 'gangbang', 'anal', 'cocksucker', 'disksucker', 'fucky', 'murder', 'kill', 'pussy', 'sucky', 'pussies', 'killing', 'asshole', 'death');
 
-// When the document is ready a click will trigger the main function
-$(document).ready(function() {
 
-  $('#check').click(function() {
-       let badWord = $('#badword').val();
-       if ($.inArray(badWord, badWords) !== false) {
-           alert(badWord + ' is in the array!');
-           badCall();
-       } else {
-           alert(badWord + ' is NOT in the array...');
-       }
-   });
+$(document).ready(setup);
 
-});
+function setup() {
+  //Calls the loading screen
+}
+
+// checkUp
+//
+// Validates if the user wrote inapropriate words or not in the search input
+function checkUp() {
+  let searchWord = $('#searchword').val();
+
+  let error = 0;
+  for (let i = 0; i < badWords.length; i++) {
+    let val = badWords[i];
+    if ((searchWord.toLowerCase()).indexOf(val.toString()) > -1) {
+      error = error + 1;
+    }
+  }
+
+  if (error > 0) {
+    badCall();
+    return false;
+
+  } else {
+    location.href = 'https://www.google.com/search?q=' + searchWord;
+  }
+}
+
 
 // badCall
 //
@@ -45,7 +61,6 @@ $(document).ready(function() {
 // by also telling out loud the bad words the user have type in
 function badCall() {
   let theuser = document.getElementById('main_input').value;
-  let thebadwords = document.getElementById('badword').value;
 
   callCount += 1;
   // Options with some randomness for variation, making it ressemble to Hal 9000
@@ -54,7 +69,7 @@ function badCall() {
     pitch: 0.9
   }
   // Say it
-  responsiveVoice.speak("I'm sorry " + theuser + ". I'm afraid I can't search for " + thebadwords, "Australian Male", options);
+  responsiveVoice.speak("I'm sorry " + theuser + ". I'm afraid I can't search for " + $('#searchword').val(), "Australian Male", options);
   // If more than three bad searches, repeat another message for the user
   if (callCount === 3) {
     setTimeout(function() {
