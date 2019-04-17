7985df4cb2f4823a9859a9580acb08fefb6d6018
Nomad29 <alexandramelancon@outlook.com>
2019-04-17 15:32:52 -0400

    Prj3: Started Project 3 with a detailed description.

---
+++ projects/project3/js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/*****************
+
+AV.ITH
+Alexandra Melançon
+
+This is a jQuery and Responsivevoice experience that intends to remake how we see and use an Antivirus on a PC in an interactive way. To eradicate virus on your computer, one must play to a tamagotchi game clone to reduce the latter's influence to be able to delete it easely.
+
+The name of this interactive software is AV.ITH (Antivirus.Information Technology Helper)
+
+Uses:
+Responsive Voice
+https://responsivevoice.org/
+jQuery
+https://jquery.com/
+
+******************/
